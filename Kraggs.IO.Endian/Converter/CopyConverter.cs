44456a138b945d2c4fa6b44215ff96d9001f1637
Jarle Hansen <jarle.hansen@gmail.com>
2015-06-16 16:22:48 +0200

    Basic EndianConverter Layout created.

---
+++ Kraggs.IO.Endian/Converter/CopyConverter.cs
@@ -0,0 +1,143 @@
+﻿#region License
+//
+// CopyConverter.cs
+// 
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Jarle Hansen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//using Mono;
+
+namespace Kraggs.IO
+{
+    internal sealed partial class CopyConverter : EndianConverter
+    {
+        #region Reading from byte Arrays
+
+        public override float ReadFloat(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double ReadDouble(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal ReadDecimal(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ushort ReadUInt16(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override uint ReadUInt32(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ulong ReadUInt64(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override short ReadInt16(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ReadInt32(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long ReadInt64(byte[] data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Writing to byte Arrays
+
+        public override void Write(byte[] dest, int index, float value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, decimal scalar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, ushort value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, uint value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, ulong value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, short value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] dest, int index, long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Kraggs.IO.Endian/Converter/EndianConverter.cs
@@ -0,0 +1,140 @@
+﻿#region License
+//
+// EndianConverter.cs
+// 
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Jarle Hansen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//using Mono;
+
+namespace Kraggs.IO
+{
+    /// <summary>
+    /// This is basically a reimplement of Mono DataConverter unfortunately.
+    /// But some of the code is changed based on performance tests.
+    /// And if possible, unsafe code is avoided in order to enable JIT optimizations.
+    /// </summary>
+    public abstract partial class EndianConverter
+    {
+        public static readonly bool IsLittleEndian = BitConverter.IsLittleEndian;
+
+        internal static readonly EndianConverter CopyConverter = new CopyConverter();
+        internal static readonly EndianConverter SwapConverter = new SwapConverter();
+
+        //public EndianConverter()
+        //{
+        //    BitConverter.
+        //}
+
+        #region Constuctors
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EndianConverter CreateLittleEndianConverter()
+        {
+            return IsLittleEndian ? CopyConverter : SwapConverter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EndianConverter CreateBitEndianConverter()
+        {
+            return IsLittleEndian ? SwapConverter : CopyConverter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EndianConverter CreateNativeConverter()
+        {
+            return CopyConverter;
+        }
+
+        //public static EndianConverter LittleEndian
+        //{
+        //    get
+        //    {
+        //        return IsLittleEndian
+        //    }
+        //}
+
+        #endregion
+
+        #region Reading from byte Arrays
+
+        // floatingpoint
+        public abstract float ReadFloat(byte[] data, int index);
+        public abstract double ReadDouble(byte[] data, int index);
+        public abstract decimal ReadDecimal(byte[] data, int index);
+
+        // unsigned integer
+        //[CLSCompliant(false)]
+        public abstract UInt16 ReadUInt16(byte[] data, int index);
+        //[CLSCompliant(false)]
+        public abstract UInt32 ReadUInt32(byte[] data, int index);
+        //[CLSCompliant(false)]
+        public abstract UInt64 ReadUInt64(byte[] data, int index);
+
+        // signed integer
+        public abstract Int16 ReadInt16(byte[] data, int index);
+        public abstract Int32 ReadInt32(byte[] data, int index);
+        public abstract Int64 ReadInt64(byte[] data, int index);
+
+        #endregion
+
+        #region Writing to byte Arrays
+
+        public abstract void Write(byte[] dest, int index, float value);
+        public abstract void Write(byte[] dest, int index, double value);
+        public abstract void Write(byte[] dest, int index, decimal scalar);
+
+        // unsigned integer
+        //[CLSCompliant(false)]
+        public abstract void Write(byte[] dest, int index, UInt16 value);
+        //[CLSCompliant(false)]
+        public abstract void Write(byte[] dest, int index, UInt32 value);
+        //[CLSCompliant(false)]
+        public abstract void Write(byte[] dest, int index, UInt64 value);
+
+        // signed integer
+        public abstract void Write(byte[] dest, int index, Int16 value);
+        public abstract void Write(byte[] dest, int index, Int32 value);
+        public abstract void Write(byte[] dest, int index, Int64 value);
+
+        #endregion
+
+        #region ReadWrite Bytes
+
+        //public abstract void ReadBytes(float[] src, int srcIndex, byte[] dst, int dstIndex);
+        //public abstract void WriteBytes(float[] dst, int dstIndex, byte[] src, int srcIndex);
+
+        #endregion
+    }
+}
