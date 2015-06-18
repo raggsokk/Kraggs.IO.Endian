dac3d3af1b59f70b4f470d3da234590987262394
Jarle Hansen <jarle.hansen@gmail.com>
2015-06-18 18:58:09 +0200

    Added commandline interface to perf testing exe.

---
+++ Kraggs.IO.Endian.PerformanceTests/PerfOptions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace Kraggs.IO.Endian.PerformanceTests
+{
+    public class PerfOptions
+    {
+        public const int BYTEARRAYSIZEMIN = 16;
+        public const int BYTEARRAYSIZEMAX = 100000000;
+        public const int BYTEARRAYDEFAULT = 20000000;
+
+        public const int RUNCOUNTMIN = 1;
+        public const int RUNCOUNTMAX = 10;
+        public const int RUNCOUNTDEFAULT = 3;
+
+        [Option('c', "count", // DefaultValue=RUNCOUNTDEFAULT, 
+            HelpText="Number of times tests are run.")]
+        public int RunCount { get; set; }
+
+        [Option('s', "size", //DefaultValue = BYTEARRAYDEFAULT, 
+            HelpText="Size of byte array to run test at")]
+        public int ByteArraySize { get; set; }
+
+        [Option("SkipReadTests", HelpText = "Skip running read tests.")]
+        public bool SkipReadTests { get; set; }
+
+        [Option("SkipWriteTests", HelpText = "Skip running write tests.")]
+        public bool SkipWriteTests { get; set; }
+
+        // reports
+
+        [Option('q', "quiet", HelpText="Dont print report results to console.")]
+        public bool Quiet { get; set; }
+
+        [Option("txtreport", HelpText = "Output a txt report to this filename")]
+        public string TextReportFilename { get; set; }
+
+        [HelpOption('h', "help")]
+        public string GetHelp()
+        {
+            var help = new HelpText()
+            {
+                AddDashesToOption = true,
+                AdditionalNewLineAfterOption = true,
+            };
+
+            help.AddPreOptionsLine(string.Format(
+                "Usage: <app> -c {0} -s {1}", RUNCOUNTDEFAULT, BYTEARRAYDEFAULT));
+            help.AddOptions(this);
+
+            return help;
+        }
+
+        public bool ValidateOptions()
+        {
+            bool flagError = false;
+
+            if(RunCount < RUNCOUNTMIN || RunCount > RUNCOUNTMAX)
+            {
+                Console.WriteLine("Error: Arg Count must be between '{0}' and '{1}'. Currently '{2}'",
+                    RUNCOUNTMIN, RUNCOUNTMAX, RunCount);
+                flagError = true;
+            }
+
+            if(ByteArraySize < BYTEARRAYSIZEMIN || ByteArraySize > BYTEARRAYSIZEMAX)
+            {
+                Console.WriteLine("Error: Arg Size mist be between '{0}' and '{1}'. Currently '{2}'",
+                    BYTEARRAYSIZEMIN, BYTEARRAYSIZEMAX, ByteArraySize);
+                flagError = true;
+            }
+
+            if(!string.IsNullOrWhiteSpace(TextReportFilename))
+            {
+                if (File.Exists(TextReportFilename))
+                {
+                    Console.WriteLine("Error: Cant write Text report to existing file '{0}'", TextReportFilename);
+                    flagError = true;
+                }
+            }
+
+            if(flagError)
+            {
+                Console.WriteLine("Maybe try '--help' for more information?");
+            }
+
+            return !flagError;
+        }
+    }
+}
--- Kraggs.IO.Endian.PerformanceTests/Program.cs
+++ Kraggs.IO.Endian.PerformanceTests/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.IO;
+using CommandLine;
+using CommandLine.Text;
 
 namespace Kraggs.IO.Endian.PerformanceTests
 {
@@ -12,75 +14,159 @@ namespace Kraggs.IO.Endian.PerformanceTests
     {
         static void Main(string[] args)
         {
-            // 1. make report
-#if DEBUG
-            var report = new TestReport(3, 20000);
-#else
-            var report = new TestReport(5, 20000000);
-#endif
-
-            // 2. add read tests
-            report.AddReadTest(ReadPerfTests.CopyReadUInt16);
-            report.AddReadTest(ReadPerfTests.SwapReadUInt16);
-            report.AddReadTest(ReadPerfTests.CopyReadUInt32);
-            report.AddReadTest(ReadPerfTests.SwapReadUInt32);
-            report.AddReadTest(ReadPerfTests.CopyReadUInt64);
-            report.AddReadTest(ReadPerfTests.SwapReadUInt64);
-
-            report.AddReadTest(ReadPerfTests.CopyReadInt16);
-            report.AddReadTest(ReadPerfTests.SwapReadInt16);
-            report.AddReadTest(ReadPerfTests.CopyReadInt32);
-            report.AddReadTest(ReadPerfTests.SwapReadInt32);
-            report.AddReadTest(ReadPerfTests.CopyReadInt64);
-            report.AddReadTest(ReadPerfTests.SwapReadInt64);
-
-            report.AddReadTest(ReadPerfTests.CopyReadFloat);
-            report.AddReadTest(ReadPerfTests.SwapReadFloat);
-            report.AddReadTest(ReadPerfTests.CopyReadDouble);
-            report.AddReadTest(ReadPerfTests.SwapReadDouble);
-
-            // 3. add write tests
-            report.AddWriteTest(WritePerfTests.CopyWriteUInt16);
-            report.AddWriteTest(WritePerfTests.SwapWriteUInt16);
-            report.AddWriteTest(WritePerfTests.CopyWriteUInt32);
-            report.AddWriteTest(WritePerfTests.SwapWriteUInt32);
-            report.AddWriteTest(WritePerfTests.CopyWriteUInt64);
-            report.AddWriteTest(WritePerfTests.SwapWriteUInt64);
-
-            report.AddWriteTest(WritePerfTests.CopyWriteInt16);
-            report.AddWriteTest(WritePerfTests.SwapWriteInt16);
-            report.AddWriteTest(WritePerfTests.CopyWriteInt32);
-            report.AddWriteTest(WritePerfTests.SwapWriteInt32);
-            report.AddWriteTest(WritePerfTests.CopyWriteInt64);
-            report.AddWriteTest(WritePerfTests.SwapWriteInt64);
-
-            report.AddWriteTest(WritePerfTests.CopyWriteFloat);
-            report.AddWriteTest(WritePerfTests.SwapWriteFloat);
-            report.AddWriteTest(WritePerfTests.CopyWriteDouble);
-            report.AddWriteTest(WritePerfTests.SwapWriteDouble);
-
-            // 4. run tests
-            report.RunTests();
-
-            // 5 generate reports
-            report.GenerateReport(new ConsoleTestReportVisitor());
-
-            // 6. generate file report too.
-#if !DEBUG
-            if (!System.Diagnostics.Debugger.IsAttached)
+            var opt = new PerfOptions();
+            if(args.Length == 0)
             {
-                var fname = Path.Combine(@"..\..\", "EndianPerformanceReport.txt");
-                //var fname = @"D:\Temp\EndianPerformanceReport.txt";
-                report.GenerateReport(new TextWriterReportVisitor(fname, true));                        
+                Console.WriteLine(opt.GetHelp());
+                Environment.Exit(0);
             }
-#endif
 
-            // 7. prevent console to exit if inside debugger.
-            if(System.Diagnostics.Debugger.IsAttached)
+            //if(CommandLine.Parser.Default.ParseArguments(args, opt))
+            if(CommandLine.Parser.Default.ParseArgumentsStrict(args, opt))
+            {
+                if(!opt.ValidateOptions())
+                {
+                    Environment.Exit(1);
+                }
+
+                var report = new TestReport(opt.RunCount, opt.ByteArraySize);
+
+
+                if (!opt.SkipReadTests)
+                {
+                    report.AddReadTest(ReadPerfTests.CopyReadUInt16);
+                    report.AddReadTest(ReadPerfTests.SwapReadUInt16);
+                    report.AddReadTest(ReadPerfTests.CopyReadUInt32);
+                    report.AddReadTest(ReadPerfTests.SwapReadUInt32);
+                    report.AddReadTest(ReadPerfTests.CopyReadUInt64);
+                    report.AddReadTest(ReadPerfTests.SwapReadUInt64);
+
+                    report.AddReadTest(ReadPerfTests.CopyReadInt16);
+                    report.AddReadTest(ReadPerfTests.SwapReadInt16);
+                    report.AddReadTest(ReadPerfTests.CopyReadInt32);
+                    report.AddReadTest(ReadPerfTests.SwapReadInt32);
+                    report.AddReadTest(ReadPerfTests.CopyReadInt64);
+                    report.AddReadTest(ReadPerfTests.SwapReadInt64);
+
+                    report.AddReadTest(ReadPerfTests.CopyReadFloat);
+                    report.AddReadTest(ReadPerfTests.SwapReadFloat);
+                    report.AddReadTest(ReadPerfTests.CopyReadDouble);
+                    report.AddReadTest(ReadPerfTests.SwapReadDouble);
+                }
+
+                if(!opt.SkipWriteTests)
+                {
+                    report.AddWriteTest(WritePerfTests.CopyWriteUInt16);
+                    report.AddWriteTest(WritePerfTests.SwapWriteUInt16);
+                    report.AddWriteTest(WritePerfTests.CopyWriteUInt32);
+                    report.AddWriteTest(WritePerfTests.SwapWriteUInt32);
+                    report.AddWriteTest(WritePerfTests.CopyWriteUInt64);
+                    report.AddWriteTest(WritePerfTests.SwapWriteUInt64);
+
+                    report.AddWriteTest(WritePerfTests.CopyWriteInt16);
+                    report.AddWriteTest(WritePerfTests.SwapWriteInt16);
+                    report.AddWriteTest(WritePerfTests.CopyWriteInt32);
+                    report.AddWriteTest(WritePerfTests.SwapWriteInt32);
+                    report.AddWriteTest(WritePerfTests.CopyWriteInt64);
+                    report.AddWriteTest(WritePerfTests.SwapWriteInt64);
+
+                    report.AddWriteTest(WritePerfTests.CopyWriteFloat);
+                    report.AddWriteTest(WritePerfTests.SwapWriteFloat);
+                    report.AddWriteTest(WritePerfTests.CopyWriteDouble);
+                    report.AddWriteTest(WritePerfTests.SwapWriteDouble);
+                }
+
+                report.RunTests();
+
+                if(!opt.Quiet)
+                {
+                    report.GenerateReport(new ConsoleTestReportVisitor());
+                }
+
+                if(!string.IsNullOrWhiteSpace(opt.TextReportFilename))
+                {
+                    report.GenerateReport(new TextWriterReportVisitor(opt.TextReportFilename));
+                }
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.WriteLine("Press any key to continue...");
                 var key = Console.ReadKey();
             }
         }
+
+//        private static void OldMain()
+//        {
+//            // 1. make report
+//#if DEBUG
+//            var report = new TestReport(3, 20000);
+//#else
+//            var report = new TestReport(5, 20000000);
+//#endif
+
+//            // 2. add read tests
+//            report.AddReadTest(ReadPerfTests.CopyReadUInt16);
+//            report.AddReadTest(ReadPerfTests.SwapReadUInt16);
+//            report.AddReadTest(ReadPerfTests.CopyReadUInt32);
+//            report.AddReadTest(ReadPerfTests.SwapReadUInt32);
+//            report.AddReadTest(ReadPerfTests.CopyReadUInt64);
+//            report.AddReadTest(ReadPerfTests.SwapReadUInt64);
+
+//            report.AddReadTest(ReadPerfTests.CopyReadInt16);
+//            report.AddReadTest(ReadPerfTests.SwapReadInt16);
+//            report.AddReadTest(ReadPerfTests.CopyReadInt32);
+//            report.AddReadTest(ReadPerfTests.SwapReadInt32);
+//            report.AddReadTest(ReadPerfTests.CopyReadInt64);
+//            report.AddReadTest(ReadPerfTests.SwapReadInt64);
+
+//            report.AddReadTest(ReadPerfTests.CopyReadFloat);
+//            report.AddReadTest(ReadPerfTests.SwapReadFloat);
+//            report.AddReadTest(ReadPerfTests.CopyReadDouble);
+//            report.AddReadTest(ReadPerfTests.SwapReadDouble);
+
+//            // 3. add write tests
+//            report.AddWriteTest(WritePerfTests.CopyWriteUInt16);
+//            report.AddWriteTest(WritePerfTests.SwapWriteUInt16);
+//            report.AddWriteTest(WritePerfTests.CopyWriteUInt32);
+//            report.AddWriteTest(WritePerfTests.SwapWriteUInt32);
+//            report.AddWriteTest(WritePerfTests.CopyWriteUInt64);
+//            report.AddWriteTest(WritePerfTests.SwapWriteUInt64);
+
+//            report.AddWriteTest(WritePerfTests.CopyWriteInt16);
+//            report.AddWriteTest(WritePerfTests.SwapWriteInt16);
+//            report.AddWriteTest(WritePerfTests.CopyWriteInt32);
+//            report.AddWriteTest(WritePerfTests.SwapWriteInt32);
+//            report.AddWriteTest(WritePerfTests.CopyWriteInt64);
+//            report.AddWriteTest(WritePerfTests.SwapWriteInt64);
+
+//            report.AddWriteTest(WritePerfTests.CopyWriteFloat);
+//            report.AddWriteTest(WritePerfTests.SwapWriteFloat);
+//            report.AddWriteTest(WritePerfTests.CopyWriteDouble);
+//            report.AddWriteTest(WritePerfTests.SwapWriteDouble);
+
+//            // 4. run tests
+//            report.RunTests();
+
+//            // 5 generate reports
+//            report.GenerateReport(new ConsoleTestReportVisitor());
+
+//            // 6. generate file report too.
+//#if !DEBUG
+//            if (!System.Diagnostics.Debugger.IsAttached)
+//            {
+//                var fname = Path.Combine(@"..\..\", "EndianPerformanceReport.txt");
+//                //var fname = @"D:\Temp\EndianPerformanceReport.txt";
+//                report.GenerateReport(new TextWriterReportVisitor(fname, true));                        
+//            }
+//#endif
+
+//            // 7. prevent console to exit if inside debugger.
+//            if(System.Diagnostics.Debugger.IsAttached)
+//            {
+//                Console.WriteLine("Press any key to continue...");
+//                var key = Console.ReadKey();
+//            }
+//        }
     }
 }
